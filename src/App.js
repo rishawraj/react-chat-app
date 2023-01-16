6d580c87fcdbb93dac9be5595ab5b616b9a23223
rishaw <rishawraj0703@gmail.com>
2023-01-16 22:11:43 +0530

    remove console.log s1

--- src/App.js
+++ src/App.js
@@ -115,6 +115,8 @@ function ChatRoom() {
 
 // ! ChatMessage
 function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
   return (
     <div className={`message ${messageClass}`}>
